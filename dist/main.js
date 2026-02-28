ee927f559f3a150caa3da27d952779b61ffbbd7e
korcevoy1994 <korcevoy.ui@gmail.com>
2026-02-28 13:34:40 +0200

    env

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,11 @@ const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const allowedOrigins = (process.env.FRONTEND_URL || 'http://localhost:3000')
+        .split(',')
+        .map((o) => o.trim());
     app.enableCors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+        origin: allowedOrigins,
         credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({
